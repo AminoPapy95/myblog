868c9cb147492e4fbb44c2af9d3bf89c9d50478d
Amine HOUSSANE <aminehoussane@gmail.com>
2021-01-20 14:29:36 +0100

    entrainement sur les routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,52 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/env',function (){
-    dd(env('DB_DATABASE'));
+/*route::get('/test',function(){
+    return 'GET route';
+});
+
+
+ va autoriser toute les méthodes
+route::any('/test',function (){
+    return "wesh les gars";
+});
+
+lorsqu'on va sur l'url test on est redirigé vers l'autre
+ route::redirect('/test','/');
+
+pour les formulaire le post
+route::post('/store',function (){
+    return 'POST route';
+});
+
+modifier une ressource
+route::put()
+
+ delete pour delete mdr
+
+ici on va récuperer l'id d'un article et autheur
+le point ? indique que c'est optionnel
+route::get('/articles/{id}/comments/{author?}', function ($id,$author = 'un bg '){
+    return $author . ' a ecrit un commentaire sur l\' article numéro '. $id;
+});
+
+*/
+
+/* cas ou plusieurs fois meme route
+    utiliser prefix
+*/
+
+route::prefix('admin/')->group(function (){
+    route::get('users',function (){
+        return 'my user list';
+    });
+
+    route::get('articles',function (){
+        return 'my articles list';
+    });
+
+    route::get('categories',function (){
+        return 'my categories list';
+    });
+
 });
