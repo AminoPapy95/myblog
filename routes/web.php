2a5bcff525f7cd0caa8858ecaccf270ecd21876b
Amine HOUSSANE <aminehoussane@gmail.com>
2021-01-28 15:01:15 +0100

    route biding optimisation

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[MainController::class,'home'])->name('home');
 
 Route::get('/articles',[MainController::class,'index'])->name('articles');
-Route::get('/articles/{slug}',[MainController::class,'show'])->name('article');
+Route::get('/articles/{article:slug}',[MainController::class,'show'])->name('article');
 
 Auth::routes();
 
